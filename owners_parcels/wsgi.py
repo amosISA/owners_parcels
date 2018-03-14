aa333a9f809ecd1cfb89ab4cf77f5cbb41568f62
amosISA <amosisa700@gmail.com>
2018-03-14 14:20:40 +0100

    Modified wsgi module

--- owners_parcels/wsgi.py
+++ owners_parcels/wsgi.py
@@ -7,10 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
+sys.path.append('/home/admin/owners_parcels/src/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "owners_parcels.settings")
+
+# prevenimos UnicodeEncodeError
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
+
+if settings.DEBUG == False:
+    # activamos nuestro virtualenv
+    activate_this = '/home/admin/owners_parcels/bin/activate_this.py'
+    execfile(activate_this, dict(__file__=activate_this))
 
 application = get_wsgi_application()
