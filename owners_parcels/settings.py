fcc479601caad63a06b4fd9500c3b5cfdc88e1f4
amosISA <amosisa700@gmail.com>
2018-03-26 14:07:52 +0200

    Ready for deploy

--- owners_parcels/settings.py
+++ owners_parcels/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gs@6t-$g!xt&bf-u63kkh)g1b7x1o^ufc-t5fe^si$qy)p4zm$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,13 +80,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'owners_parcels',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'OPTIONS': {
-            'sql_mode': 'traditional'
-        },
+        'USER': 'amos',
+        'PASSWORD': DBPASS,
+        # 'HOST': 'localhost',
+        # 'PORT': 3306,
+        # 'OPTIONS': {
+        #     'sql_mode': 'traditional'
+        # },
     }
 }
 
